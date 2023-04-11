b420a64c683f6dd1ecdfa0446f5a12a12ef3f1b6
victorifpizza <rm550609@fiap.com.br>
2023-04-11 10:10:30 -0300

    add  comandos

--- sprint.py
+++ sprint.py
@@ -1,1 +1,10 @@
-# print, input, atribuição de variaveis, operadores de comparação, fstring com resultados, informar  requisitos de preenchimentos.
+# print, input, atribuição de variaveis, operadores de comparação, fstring com resultados, informar requisitos de preenchimentos.
+
+print("Gostara de saber qual é a nossa ODS?")
+resposta = input()
+
+if resposta == "sim":
+    ods = 11
+    print(f"Tornar as cidades e comunidades mais inclusiva, seguras, resilientes e sustentáveis: {ods}")
+
+print()
