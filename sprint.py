e4a71801862b8cd06c20d88072c18230848d9e47
victorifpizza <rm550609@fiap.com.br>
2023-04-11 10:26:16 -0300

    entregando

--- sprint.py
+++ sprint.py
@@ -1,5 +1,5 @@
 
-print('''Gostara de saber qual é a nossa ODS?
+print('''Gostara de saber qual é a nossa proposta de solução?
 
 [1] sim
 [2] não
@@ -9,6 +9,6 @@ resposta = input()
 
 if resposta == 1:
     ods = 11.6
-    print(f"Tornar as cidades e comunidades mais inclusiva, seguras, resilientes e sustentáveis: {ods}")
+    print(f"A prposta de solução tem o foco na reciclagem. E a ods é tornar as cidades e comunidades mais inclusiva, seguras, resilientes e sustentáveis: {ods}")
 else:
     print("que pena!")
