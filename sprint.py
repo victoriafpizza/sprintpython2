1c3e5938f25d657693417eebf7e20a6f4d281d3b
emanuelle <rm97973@fiap.com.br>
2023-08-25 14:15:02 -0300

    lerolero

--- sprint.py
+++ sprint.py
@@ -17,19 +17,21 @@ usuarios = {
     }
 }
 
-# Loop para validação do usuário
-while True:
-    print("Por favor insira seu PIN.")
-    pin = input()  # Lê o PIN como string
+# Criando aa função de validação de usuário
+def validacao_usuario() :
+    while True:
+        print("Por favor insira seu PIN.")
+        pin = input()  # Lê o PIN como string
 
-    if pin in usuarios.values():
-        for usuario, dados in usuarios.items():
-            if dados["Pin"] == pin:
-                print("Bem-vindo, " + usuario + "!")
-                break
-    else:
-        print("Não é cliente da reUse?! Se cadastre através do QR CODE abaixo!\nLembre-se, para descartar o lixo não é preciso ser cadastrado no sistema! Você só precisa despejá-lo no outro lado da lixeira :)")
-        continue  # Volta ao início do loop para tentar novamente
+        if pin in usuarios.values():
+            for usuario, dados in usuarios.items():
+                if dados["Pin"] == pin:
+                    print("Bem-vindo, " + usuario + "!")
+                    break
+        else:
+            print("Não é cliente da reUse?! Se cadastre através do QR CODE abaixo!\nLembre-se, para descartar o lixo não é preciso ser cadastrado no sistema! Você só precisa despejá-lo no outro lado da lixeira :)")
+            continue  # Volta ao início do loop para tentar novamente
+
 
 
 
