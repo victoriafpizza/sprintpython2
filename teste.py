01f9702c9503482f6bd9bc0a4f89ff4d483e5585
emanuelle <rm97973@fiap.com.br>
2023-08-28 12:51:49 -0300

    agora ach q vai

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 # Importando bibliotecas
 import random
+import re
 
 # Definindo usuários
 usuarios = {
@@ -25,6 +26,10 @@ usuarios = {
 
 # Definindo funções que serão utilizadas no programa
 
+# Gerando pins aleatórios para usuários novos
+def gerar_pin_aleatorio():
+    pin = ''.join(str(random.randint(0, 9)) for _ in range(5))
+    return pin
 
 # Programa principal 
 
@@ -32,3 +37,36 @@ usuarios = {
 print("*************************")
 print("****Bem Vindo a reUse****")
 print("*************************")
+
+# Exibindo o menu inicial primário
+print("Escolha uma opção:\n(1) LogIn\n(2) SignIn")
+escolha_menu_inicial = int(input())
+
+if escolha_menu_inicial == 1 :
+    while True:
+        print("Por favor insira seu PIN de 5 números.")
+        pin = input()
+
+        if pin in usuarios.values():
+            for usuario, dados in usuarios.items():
+                if dados["Pin"] == pin:
+                    print(f"Bem-vindo, {usuario}! Estamos felizes em receber de volta.")
+                    break
+        else:
+            print("Não é cliente da reUse?! Escolha a opção de cadastro!\nLembre-se, para descartar o lixo não é preciso ser cadastrado no sistema! Você só precisa despejá-lo no outro lado da lixeira :)")
+            continue
+elif escolha_menu_inicial == 2 :
+    print("Por Favor, insira seu nome:")
+    nome_usuario = input()
+    if re.search("\d",nome_usuario) :
+        erro = "Nomes não podem conter números."
+        raise ValueError
+    print(f"Ok ")
+    # Fazendo um sorteio p\ gerar o pin (com 5 num)
+    pin_aleatorio = gerar_pin_aleatorio()
+    
+    print(f"Bem vindx {nome_usuario}!\n o seu pin será: {pin_aleatorio}. Lembre-se, ele é unico, guarde ele com carinho :)")
+else : 
+    print("")
+
+    
